dbd308ef67db91ed9d994ea3cbee3103ab90cb50
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-07-09 14:28:51 +0200

    Initial commit

---
+++ src/main/java/niccolosciucco/u5_w2_d4/U5W2D4Application.java
@@ -0,0 +1,13 @@
+package niccolosciucco.u5_w2_d4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W2D4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W2D4Application.class, args);
+	}
+
+}
---
+++ src/test/java/niccolosciucco/u5_w2_d4/U5W2D4ApplicationTests.java
@@ -0,0 +1,13 @@
+package niccolosciucco.u5_w2_d4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
